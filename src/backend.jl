9c68f160cb530f77d70f3c3c4494fe0bc7c1325c
Ubuntu <ubuntu@ip-10-63-5-83.ec2.internal>
2013-12-10 20:08:29 +0000

    using repr

--- src/backend.jl
+++ src/backend.jl
@@ -10,6 +10,6 @@ while true
    print("Waiting for input")
    msg = ZMQ.recv(io)
    print(bytestring(msg))
-   resp = string(eval(parse(bytestring(msg))))
+   resp = repr(eval(parse(bytestring(msg))))
    ZMQ.send(io, resp)
 end
